8b4ca92925116786d397ed64dd948e37c08879b5
Bruno Vicente <brunnovicente@gmail.com>
2024-11-12 13:59:43 -0300

    Primeiros códigos

---
+++ app/src/main/java/com/br/tarefas1/PrincipalActivity.java
@@ -0,0 +1,32 @@
+package com.br.tarefas1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_principal);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void cadastrar(View v){
+        Intent tela_cadastro = new Intent(this, CadastroActivity.class);
+        startActivity(tela_cadastro);
+    }
+
+}
